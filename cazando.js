01bd42b0a130800e158a0a37b02f25980bbddc6a
mvcobos <vcobos178@gmail.com>
2026-09-19 12:26:51 -0500

    Mover gato en todas direcciones

--- cazando.js
+++ cazando.js
@@ -43,3 +43,24 @@ function moverIzquierda(){
     graficarGato();
     graficarComida();
 }
+
+function moverDerecha(){
+    gatoX+=10;
+    limpiarCanva();
+    graficarGato();
+    graficarComida();
+}
+
+function moverArriba(){
+    gatoY-=10;
+    limpiarCanva();
+    graficarGato();
+    graficarComida();
+}
+
+function moverAbajo(){
+    gatoY+=10;
+    limpiarCanva();
+    graficarGato();
+    graficarComida();
+}
